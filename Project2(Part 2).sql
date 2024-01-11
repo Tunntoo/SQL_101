a26ff2a958d8468341ebfa975f060469134694ca
Giang Pham <35312690+Tunntoo@users.noreply.github.com>
2024-01-10 22:43:36 -0800

    Project2(Part 2).sql

--- Project2(Part 2).sql
+++ Project2(Part 2).sql
@@ -38,7 +38,7 @@ from main
 ---cohort analysis
 with index as (select 
 user_id,
-extract(year from created_at)||'-'||extract(month from created_at) as cohort_date,
+format_date('%Y-%m',first_purchase_date) as cohort_date,
 created_at,
 12*(extract(year from created_at) - extract(year from first_purchase_date))+
 (extract(month from created_at) - extract(month from first_purchase_date)) + 1 as indexx
@@ -46,8 +46,7 @@ from(
 select user_id,
 min(created_at) over(partition by user_id) as first_purchase_date,
 created_at
-from bigquery-public-data.thelook_ecommerce.order_items
-where created_at BETWEEN '2019-01-01' AND '2022-05-01' and status = 'Complete'))
+from bigquery-public-data.thelook_ecommerce.order_items))
 
 , cohort as (select 
 cohort_date,
@@ -64,6 +63,16 @@ sum(case when indexx=3 then cnt else 0 end) as m3,
 sum(case when indexx=4 then cnt else 0 end) as m4
 from cohort
 group by cohort_date)
+
+---retention cohort
+select cohort_date,
+round(m1/m1*100.00,2)||'%' as m1,
+round(m2/m1*100.00,2)||'%' as m2,
+round(m3/m1*100.00,2)||'%' as m3,
+round(m4/m1*100.00,2)||'%' as m4,
+from customer_cohort
+
+
 
 ---retention cohort
 select cohort_date,
